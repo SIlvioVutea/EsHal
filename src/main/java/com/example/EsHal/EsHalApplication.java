3793d832eaf2560451c1d436492b7a8310f88606
SIlvioVutea <silviovutea@gmail.com>
2024-05-26 13:27:45 +0200

    done

---
+++ src/main/java/com/example/EsHal/EsHalApplication.java
@@ -0,0 +1,13 @@
+package com.example.EsHal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsHalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsHalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EsHal/EsHalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsHal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsHalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
